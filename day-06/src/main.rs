d1ee8b4b3e57310f7dee15a9003a8fbe1a213326
Malte Tammena <malte.tammena@pm.me>
2023-12-07 22:53:26 +0100

    improve code for 06

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -1,5 +1,7 @@
 #![feature(isqrt)]
-use aoc_utils::{Error, Lines};
+use std::num::ParseIntError;
+
+use aoc_utils::{Error, Lines, Result};
 
 struct Day06;
 
@@ -10,49 +12,18 @@ impl aoc_utils::Problem<Lines> for Day06 {
         let first = input
             .next()
             .ok_or_else(|| Error::input("Where is the first line?"))??;
-        let second = input
-            .next()
-            .ok_or_else(|| Error::input("Where is the second line?"))??;
         let first = first
             .split_ascii_whitespace()
             .skip(1)
             .map(str::parse::<usize>);
+        let second = input
+            .next()
+            .ok_or_else(|| Error::input("Where is the second line?"))??;
         let second = second
             .split_ascii_whitespace()
             .skip(1)
             .map(str::parse::<usize>);
-        first
-            .zip(second)
-            .try_fold(1_usize, |product, (total_time, winning_distance)| {
-                let total_time = total_time?;
-                let winning_distance = winning_distance?;
-                // Could be 0.5 too small
-                let hodl_end = (total_time.pow(2).saturating_sub(4 * winning_distance)).isqrt() / 2;
-                // Could be 0.5 too small
-                let hodl_start = total_time / 2;
-                // Could be 0.5 too small or too large
-                let hodl_min = hodl_start - hodl_end;
-                // Could be 1.0 too small
-                let hodl_max = hodl_start + hodl_end;
-                // Could be 1.5 too small or 0.5 too large
-                let mut ways_to_win = hodl_max - hodl_min + 1;
-                if distance(hodl_min, total_time) <= winning_distance {
-                    ways_to_win -= 1
-                }
-                if distance(hodl_max, total_time) <= winning_distance {
-                    ways_to_win -= 1
-                }
-                if hodl_min > 0 && distance(hodl_min - 1, total_time) > winning_distance {
-                    ways_to_win += 1
-                }
-                if distance(hodl_max + 1, total_time) > winning_distance {
-                    ways_to_win += 1
-                }
-                if distance(hodl_max + 2, total_time) > winning_distance {
-                    ways_to_win += 1
-                }
-                Ok(product * ways_to_win)
-            })
+        first.zip(second).try_fold(1_usize, add_factor)
     }
 
     fn solve_second(mut input: Lines) -> aoc_utils::Result<Self::Solution> {
@@ -70,37 +41,46 @@ impl aoc_utils::Problem<Lines> for Day06 {
             .bytes()
             .filter(|byte| byte.is_ascii_digit())
             .fold(0_usize, |num, digit| num * 10 + (digit - b'0') as usize);
-        // Could be 0.5 too small
-        let hodl_end = (total_time.pow(2).saturating_sub(4 * winning_distance)).isqrt() / 2;
-        // Could be 0.5 too small
-        let hodl_start = total_time / 2;
-        // Could be 0.5 too small or too large
-        let hodl_min = hodl_start - hodl_end;
-        // Could be 1.0 too small
-        let hodl_max = hodl_start + hodl_end;
-        // Could be 1.5 too small or 0.5 too large
-        let mut ways_to_win = hodl_max - hodl_min + 1;
-        if distance(hodl_min, total_time) <= winning_distance {
-            ways_to_win -= 1
-        }
-        if distance(hodl_max, total_time) <= winning_distance {
-            ways_to_win -= 1
-        }
-        if hodl_min > 0 && distance(hodl_min - 1, total_time) > winning_distance {
-            ways_to_win += 1
-        }
-        if distance(hodl_max + 1, total_time) > winning_distance {
-            ways_to_win += 1
-        }
-        if distance(hodl_max + 2, total_time) > winning_distance {
-            ways_to_win += 1
-        }
-        Ok(ways_to_win)
+        add_factor(1, (Ok(total_time), Ok(winning_distance)))
     }
 }
 
-fn distance(hodl: usize, total_time: usize) -> usize {
-    hodl * (total_time.saturating_sub(hodl))
+fn add_factor(
+    product: usize,
+    (total_time, winning_distance): (Result<usize, ParseIntError>, Result<usize, ParseIntError>),
+) -> Result<usize> {
+    let total_time = total_time?;
+    let winning_distance = winning_distance?;
+    // Could be 0.5 too small
+    let hold_end = (total_time.pow(2).saturating_sub(4 * winning_distance)).isqrt() / 2;
+    // Could be 0.5 too small
+    let hold_start = total_time / 2;
+    // Could be 0.5 too small or too large
+    let hold_min = hold_start - hold_end;
+    // Could be 1.0 too small
+    let hold_max = hold_start + hold_end;
+    // Could be 1.5 too small or 0.5 too large
+    let mut ways_to_win = hold_max - hold_min + 1;
+    if distance(hold_min, total_time) <= winning_distance {
+        ways_to_win -= 1
+    }
+    if distance(hold_max, total_time) <= winning_distance {
+        ways_to_win -= 1
+    }
+    if hold_min > 0 && distance(hold_min - 1, total_time) > winning_distance {
+        ways_to_win += 1
+    }
+    if distance(hold_max + 1, total_time) > winning_distance {
+        ways_to_win += 1
+    }
+    if distance(hold_max + 2, total_time) > winning_distance {
+        ways_to_win += 1
+    }
+    Ok(product * ways_to_win)
+}
+
+fn distance(hold: usize, total_time: usize) -> usize {
+    hold * (total_time.saturating_sub(hold))
 }
 
 aoc_utils::main!(Day06, Lines, "inputs-06-test" => 288, "inputs-06-test" => 71503);
